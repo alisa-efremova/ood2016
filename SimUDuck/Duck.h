d7cb52a3221e9506e6eda2bc00905b4da1288ec7
alisa-teplouhova <flovettee@gmail.com>
2016-03-27 16:50:44 +0300

    Убрала using namespace std из Duck.h

--- SimUDuck/Duck.h
+++ SimUDuck/Duck.h
@@ -5,22 +5,20 @@
 #include "IQuackBehavior.h"
 #include "IDanceBehavior.h"
 
-using namespace std;
-
 class Duck
 {
 public:
-	Duck(unique_ptr<IFlyBehavior> && flyBehavior, unique_ptr<IQuackBehavior> && quackBehavior, unique_ptr<IDanceBehavior> && danceBehavior);
+	Duck(std::unique_ptr<IFlyBehavior> && flyBehavior, std::unique_ptr<IQuackBehavior> && quackBehavior, std::unique_ptr<IDanceBehavior> && danceBehavior);
 	virtual ~Duck();
 	void Quack() const;
 	void Swim();
 	void Fly();
 	void Dance();
-	void SetFlyBehavior(unique_ptr<IFlyBehavior> && flyBehavior);
+	void SetFlyBehavior(std::unique_ptr<IFlyBehavior> && flyBehavior);
 	virtual void Display() const = 0;
 
 private:
-	unique_ptr<IFlyBehavior> m_flyBehavior;
-	unique_ptr<IQuackBehavior> m_quackBehavior;
-	unique_ptr<IDanceBehavior> m_danceBehavior;
+	std::unique_ptr<IFlyBehavior> m_flyBehavior;
+	std::unique_ptr<IQuackBehavior> m_quackBehavior;
+	std::unique_ptr<IDanceBehavior> m_danceBehavior;
 };
